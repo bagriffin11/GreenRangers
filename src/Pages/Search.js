a2daac9cc66e88624b337c5d28cf091ab5e74c21
bagriffin11 <48770963+bagriffin11@users.noreply.github.com>
2022-09-04 19:57:52 -0700

    Search page successful
    This page successfully stores the zip codes from a 10 mile radius from the users zip code and stores it into a react state variable. This variable can be used to transfer data (zip code, longitude and latitude) to the map page.

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -27,23 +27,45 @@ function Search() {
     let navigate = useNavigate();
     const [zipCode, setZipCode] = useState("");
 
-    const [zipCodes, setZipCodes] = useState({});
+    const [zipCodes, setZipCodes] = useState([]);
     const [response, setResponse] = useState(false);
 
     const searched = async () => {
         setResponse(true);
     };
+    const list = zipCodes.map((zip, index) => {
+        return (
+          <div key={index}>
+           <p>{index+1}. {zip} </p>
+          </div>
+        );
+      }); 
+    
+
 
     useEffect(() => {            
         const apikey = "5HWN21OEYZWOCCMBEQ49"
         const zipApi = "https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=" + zipCode + "&minimumradius=0&maximumradius=10&country=ALL&key=" + apikey;
         const getUserSS = async () => {
             
-               const zips = await fetch(zipApi);
+            const zips = await fetch(zipApi);
             const zipresp = await zips.json();
             console.log(zipresp);
-            setZipCodes(zipresp); 
-            
+            var sub_val = new Array(zipresp.length); 
+            for (var i in zipresp) {
+				var key = i;
+				var val = zipresp[i];
+                for (var j in val) {
+					var sub_key = j;
+					sub_val[j] = val[j].Code;
+                    console.log(sub_val[j])
+                    
+                }
+                
+            }
+            console.log(sub_val);
+            setZipCodes(sub_val);
+
         };
         if (response === true){
             getUserSS();
@@ -70,12 +92,13 @@ function Search() {
 <button onClick={searched}>Look up</button>
 
       <h3>{zipCode} .. 
-      {response === true  ? <div> <p>true  </p></div>:<p></p> }</h3>
+      {response === true  ? <div>{list} </div>:<div><p>ok</p></div>}</h3>
       <div>
       
       </div>
+     
         </div>
-			
+	
         </div>
     );
 }
